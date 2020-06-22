b75c7d87a8269f41e07818d6c3af5b6841b5c064
Emeka <ilozuechukwuemeka@yahoo.com>
2020-06-22 23:32:00 +0100

    Testing Views That Use Adapters like spinners

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -4,6 +4,7 @@ import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,21 +15,40 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetUp(){
+        sDataManager = DataManager.getInstance();
+    }
+
     @Rule
     public ActivityTestRule<NoteListActivity> mNoteListActivityActivityTestRule =
             new ActivityTestRule<>(NoteListActivity.class);
 
     @Test
     public void createNewNote(){
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of our test note";
+
         //ViewInteraction fabNewNote = onView(withId(R.id.fab));
         //fabNewNote.perform(click());
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("This is the body of our test note"),
+        onView(withId(R.id.spinner_courses)).perform(click());
+
+        onData(allOf(instanceOf(CourseInfo.class),equalTo(course))).perform(click());
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
                 closeSoftKeyboard() );
+
+        pressBack();
     }
 
 }
