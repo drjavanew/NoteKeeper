1022d4f671e15d1ae95ea99bb50563ab97dd040e
Emeka <ilozuechukwuemeka@yahoo.com>
2020-06-23 00:59:50 +0100

    UI Testing using espresso & android studio test recorder

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -17,6 +17,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static org.hamcrest.Matchers.*;
 import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
@@ -34,21 +35,32 @@ public class NoteCreationTest {
     @Test
     public void createNewNote(){
         final CourseInfo course = sDataManager.getCourse("java_lang");
-        final String noteTitle = "Test note title";
+        final String noteTitle = "test Note title";
         final String noteText = "This is the body of our test note";
 
         //ViewInteraction fabNewNote = onView(withId(R.id.fab));
         //fabNewNote.perform(click());
         onView(withId(R.id.fab)).perform(click());
         onView(withId(R.id.spinner_courses)).perform(click());
-
         onData(allOf(instanceOf(CourseInfo.class),equalTo(course))).perform(click());
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(course.getTitle()))));
 
-        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+                .check(matches(withText(containsString(noteTitle))));
+
         onView(withId(R.id.text_note_text)).perform(typeText(noteText),
-                closeSoftKeyboard() );
+                closeSoftKeyboard());
+
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
 
         pressBack();
-    }
 
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+
+    }
 }
