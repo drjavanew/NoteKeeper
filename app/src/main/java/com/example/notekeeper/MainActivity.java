304222ca95c0d2390a89ca90fadc6e3ec24d3038
Emeka <ilozuechukwuemeka@yahoo.com>
2020-06-30 13:52:50 +0100

    Using String resources in java code

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -137,17 +137,17 @@ public class MainActivity extends AppCompatActivity
         } else if(id == R.id.nav_courses){
             displayCourses();
         } else if(id == R.id.nav_share){
-            handleSelection("Don't you think you've shared enough");
+            handleSelection(R.string.nav_share_message);
         } else if(id == R.id.nav_send){
-            handleSelection("Send");
+            handleSelection(R.string.nav_send_message);
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    private void handleSelection(String message) {
+    private void handleSelection(int message_id) {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view, message_id, Snackbar.LENGTH_LONG).show();
     }
 }
