2fd3d188ad13385c3dc8622aaa498baeb1d9f6ea
Emeka <ilozuechukwuemeka@yahoo.com>
2020-08-21 12:38:26 +0100

    Creating and displaying Notification by customizing a notification helper class

--- app/src/main/java/com/example/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -287,10 +287,7 @@ public class NoteActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+       int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_send_mail) {
@@ -301,9 +298,15 @@ public class NoteActivity extends AppCompatActivity
             finish();
         } else if(id == R.id.action_next){
             moveNext();
+        } else if(id == R.id.action_set_reminder){
+            showReminderNotification();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showReminderNotification() {
+        NoteReminderNotification.notify(this, "Title", "This is dummy text", 0);
     }
 
     @Override
---
+++ app/src/main/java/com/example/notekeeper/NoteReminderNotification.java
@@ -0,0 +1,134 @@
+package com.example.notekeeper;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+/**
+ * Helper class for showing and canceling note reminder
+ * notifications.
+ * <p>
+ * This class makes heavy use of the {@link NotificationCompat.Builder} helper
+ * class to create notifications in a backward-compatible way.
+ */
+public class NoteReminderNotification {
+    /**
+     * The unique identifier for this type of notification.
+     */
+    private static final String NOTIFICATION_TAG = "NoteReminder";
+    private static final String CHANNEL_ID = "default";
+    private static final String CHANNEL_DESCRIPTION = "Your description...";
+    private static int importance = NotificationManager.IMPORTANCE_DEFAULT;
+    private static CharSequence name = "NoteKeeper";
+
+
+    /**
+     * Shows the notification, or updates a previously shown notification of
+     * this type, with the given parameters.
+     * <p>
+     * TODO: Customize this method's arguments to present relevant content in
+     * the notification.
+     * <p>
+     * TODO: Customize the contents of this method to tweak the behavior and
+     * presentation of note reminder notifications. Make
+     * sure to follow the
+     * <a href="https://developer.android.com/design/patterns/notifications.html">
+     * Notification design guidelines</a> when doing so.
+     *
+     * @see #cancel(Context)
+     */
+    public static void notify(final Context context,
+                              final String noteTitle, final String noteText, int noteId) {
+        final Resources res = context.getResources();
+        createNotificationChannel(context);
+
+        // This image is used as the notification's large icon (thumbnail).
+        // TODO: Remove this if your notification has no relevant thumbnail.
+        final Bitmap picture = BitmapFactory.decodeResource(res, R.drawable.logo);
+
+        Intent noteActivityIntent = new Intent(context, NoteActivity.class);
+        noteActivityIntent.putExtra(NoteActivity.NOTE_ID, noteId);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setSmallIcon(R.drawable.ic_stat_note_reminder)
+                .setLargeIcon(picture)
+                .setContentTitle("Review note")
+                .setContentText(noteText)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT).setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(noteText)
+                        .setBigContentTitle(noteTitle)
+                        .setSummaryText("Review note"))
+                .setContentIntent(
+                        PendingIntent.getActivity(
+                                context,
+                                0,
+                                noteActivityIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT))
+                .addAction(
+                        0,
+                        "View all notes",
+                        PendingIntent.getActivity(
+                                context,
+                                0,
+                                new Intent(context, MainActivity.class),
+                                PendingIntent.FLAG_UPDATE_CURRENT))
+
+                // Automatically dismiss the notification when it is touched.
+                .setAutoCancel(true);
+
+        notify(context, builder.build());
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    private static void notify(final Context context, final Notification notification) {
+        final NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            nm.notify(NOTIFICATION_TAG, 0, notification);
+        } else {
+            nm.notify(NOTIFICATION_TAG.hashCode(), notification);
+        }
+    }
+
+    /**
+     * Cancels any notifications of this type previously shown using
+     * {@link # notify(Context, String, int)}.
+     */
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    public static void cancel(final Context context) {
+        final NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            nm.cancel(NOTIFICATION_TAG, 0);
+        } else {
+            nm.cancel(NOTIFICATION_TAG.hashCode());
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static void createNotificationChannel(final Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(CHANNEL_DESCRIPTION);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = (NotificationManager) context
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
