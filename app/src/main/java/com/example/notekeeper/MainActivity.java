17fbc5043a6580bd12f42e3de9b89bd76fd2f435
Emeka <ilozuechukwuemeka@yahoo.com>
2020-08-25 00:46:13 +0100

    App Note Backup Feature

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -247,9 +247,15 @@ public class MainActivity extends AppCompatActivity
         if(id == R.id.action_settings){
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
+        } else if (id == R.id.action_backup_notes){
+            backupNotes();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void backupNotes() {
+        NoteBackup.doBackup(MainActivity.this, NoteBackup.ALL_COURSES);
     }
 
     @Override
